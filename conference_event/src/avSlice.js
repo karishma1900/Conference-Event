c2cf868d680177a2b16c1a8d034bac3b33a86f11
Karishma's Classes <156506602+karishma1900@users.noreply.github.com>
2024-08-28 17:36:38 +0530

    Update avSlice.js

--- conference_event/src/avSlice.js
+++ conference_event/src/avSlice.js
@@ -1,35 +1,40 @@
 import { createSlice } from "@reduxjs/toolkit";
+import projector from './assets/projector.jpg'
+import signage from './assets/signage.jpg'
+import speaker from './assets/speakers.jpg'
+import whiteboard from './assets/whiteboar.webp'
+import microphone from './assets/microphones.jpg'
 
 export const avSlice = createSlice({
   name: "av",
   initialState: [
         {
-        img: "",
+        img: "projector",
         name: "Projectors",
         cost: 200,
         quantity: 0,
     },
     {
-        img: "",
+        img: "speaker",
         name: "Speaker",
         cost: 35,
         quantity: 0,
     },
     {
-        img: "",
+        img: "microphone",
         name: "Microphones",
         cost: 45,
         quantity: 0,
     },
     {
-        img: "",
+        img: "whiteboard",
         name: "Whiteboards",
         cost: 80,
         quantity: 0,
     },
 
     {
-        img: "",
+        img: "signage",
         name: "Signage",
         cost: 80,
         quantity: 0,
